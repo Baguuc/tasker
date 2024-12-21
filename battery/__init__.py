828b00ecaeef29e87e2970e2677b88268d5566b0
Jan Zukowski <janzukowski@proton.me>
2024-12-21 14:14:43 +0100

    update: added BatteryInfo class

---
+++ battery/__init__.py
@@ -0,0 +1,19 @@
+import psutil
+from typing import Self
+from dataclasses import dataclass
+from scriptlib.time import Time
+
+
+@dataclass
+class BatteryInfo:
+    percentage: float
+    plugged_in: bool
+    time_left: Time
+
+    def get_data() -> Self:
+        data = psutil.sensors_battery()
+        time_left: Time = Time.from_seconds(data.secsleft)
+
+        return BatteryInfo(data.percent, data.power_plugged, time_left)
+
+
