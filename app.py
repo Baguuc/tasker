0f659474ddd811572fb9e65d1ce2b2c2e923e347
Jan Zukowski <janzukowski@proton.me>
2024-12-21 21:53:34 +0100

    update: added aligned printing

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from lib.battery import BatteryInfo
-from lib.console import ConsoleColor
+from lib.console import ConsoleColor, Console, Alignment
 from lib.tasks import Task
 import sys
 
@@ -17,16 +17,30 @@ battery: BatteryInfo = BatteryInfo.get_data()
 
 if battery.plugged_in:
     info_str: str = ConsoleColor.color("Battery on charge", ConsoleColor.YELLOW)
-    print(f"{info_str}.")
+    Console.print_aligned(info_str, Alignment.Center)
 else:
     prct_str: str = ConsoleColor.color(f"{battery.percentage:.0f}%", ConsoleColor.YELLOW)
     time_str: str = ConsoleColor.color(f"{battery.time_left} hours", ConsoleColor.YELLOW)
 
-    print(f"Battery {prct_str}, {time_str} of usage left.")
+    Console.print_aligned(f"Battery {prct_str}, {time_str} of usage left", Alignment.Center)
 
-print(ConsoleColor.color("Current task:", ConsoleColor.BOLD))
+print()
+
+Console.print_aligned(
+    ConsoleColor.color(
+        "Current task:",
+        ConsoleColor.BOLD
+    ),
+    Alignment.Center
+)
 current_task: Task = Task.get_current()
-print(f"{current_task.title}")
+Console.print_aligned(
+    current_task.title,
+    Alignment.Center
+)
 for line in current_task.details:
-    print(f"{ConsoleColor.color('-', ConsoleColor.GREEN)} {line}")
+    Console.print_aligned(
+        line,
+        Alignment.Center
+    )
 
--- lib/console.py
+++ lib/console.py
@@ -1,4 +1,6 @@
 import subprocess
+import re
+from enum import Enum, auto
 
 class ConsoleColor:
     CYAN: str = '\033[96m'
@@ -12,10 +14,51 @@ class ConsoleColor:
     ENDC: str = '\033[0m'
 
     def color(s: str, color_code: str) -> str:
-        return f"{color_code}{s}{ConsoleColor.ENDC}"
+        colored: str = f"{color_code}{s}{ConsoleColor.ENDC}"
+        
+        return colored
+
+class Alignment(Enum):
+    Left = auto()
+    Center = auto()
+    Right = auto()
 
 class Console:
     def get_max_columns() -> int:
         tput_cols = subprocess.check_output(['tput', 'cols']).decode().strip()
+        cols: int = int(tput_cols)
         
-        return tput_cols
+        if cols % 2 != 0:
+            cols -= 1
+
+        return cols
+    
+    def print_aligned(s: str, alignment: Alignment):
+        if alignment == Alignment.Left:
+            print(s)
+            return
+
+        column_count: int = Console.get_max_columns()
+        slen: int = len(s)
+        COLOR_CODE_PATTERN = r'(\x1B(?:\[([0-9]{1,2}(;[0-9]{1,2})?)m|))'       
+        color_occurences: list = re.findall(COLOR_CODE_PATTERN, s)
+        total_color_len: int = 0
+
+        for occurence in color_occurences:
+            total_color_len += len(occurence[0])
+
+        slen -= total_color_len
+
+        if alignment == Alignment.Center:
+            pad_times: int = (column_count - slen) // 2
+            pad: str = " " * pad_times
+            print(f"{pad}{s}")
+            
+            return
+
+        if alignment == Alignment.Right:
+            pad_times: int = (column_count - slen)
+            pad: str = " " * pad_times
+            print(f"{pad}{s}")
+
+            return
