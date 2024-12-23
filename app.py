563c4e26dbd042e0e8c836d2846f22233ff575ef
Jan Zukowski <janzukowski@proton.me>
2024-12-23 12:18:37 +0100

    update: cleaned up app.py

--- app.py
+++ app.py
@@ -3,64 +3,57 @@ from lib.tasks import Task
 import sys
 import os
 
-def main(argc, argv):
-    if argc == 2 and argv[0] == "tasks" and argv[1] == "insert":
-        title: str = input("Enter title of the task: ")
-        details: list[str] = []
 
-        while (line := input("Enter next line of details (leave blank to exit): ")) != "":
-           details.append(line)
-        
-        Task.insert(title, details)
+def insert(argc, argv):
+    title: str = input("Enter title of the task: ")
+    details: list[str] = []
 
-        Console.clear()
-        main(0, [])
+    while (line := input("Enter next line of details (leave blank to exit): ")) != "":
+        details.append(line)
         
+    Task.insert(title, details)
+
+
+def done(argc, argv):
+    Task.mark_current_done()
+
+
+def update(argc, argv):
+    try:
+        _id: int = int(argv[2])
+    except:
+        print("The id has to be numeric")
         exit()
-
-    elif argc == 2 and argv[0] == "tasks" and argv[1] == "done":
-        Task.mark_current_done()
-        Console.clear()
-
-        main(0, [])
-        exit()
-    
-    elif argc == 3 and argv[0] == "tasks" and argv[1] == "update":
-        try:
-            _id: int = int(argv[2])
-        except:
-            print("The id has to be numeric")
-            exit()
         
-        current_data: Task = Task.select_one(_id)
-        print(ConsoleColor.color(current_data.title, ConsoleColor.YELLOW))
-        title: str = input("Enter new title of the task: ")
-        details: list[str] = []
+    current_data: Task = Task.select_one(_id)
+    print(ConsoleColor.color(current_data.title, ConsoleColor.YELLOW))
+    title: str = input("Enter new title of the task: ")
+    details: list[str] = []
         
-        linei: int = 0
+    linei: int = 0
+    try:
+        print(ConsoleColor.color(current_data.details[linei], ConsoleColor.YELLOW))
+    except:
+        pass
+
+    line = input("Enter next line of new details (leave blank to exit): ")
+        
+    while line != "":
+        linei += 1
+        details.append(line)
+
+        
         try:
             print(ConsoleColor.color(current_data.details[linei], ConsoleColor.YELLOW))
         except:
             pass
 
         line = input("Enter next line of new details (leave blank to exit): ")
-        
-        while line != "":
-            linei += 1
-            details.append(line)
+    
+    Task.update(_id, title, details)
 
-        
-            try:
-                print(ConsoleColor.color(current_data.details[linei], ConsoleColor.YELLOW))
-            except:
-                pass
 
-            line = input("Enter next line of new details (leave blank to exit): ")
-        Task.update(_id, title, details)
-        Console.clear()
-        main(0, [])
-        exit()
-
+def main(argc, argv):
     current_task: Task = Task.get_current()
     Console.print_aligned(
         ConsoleColor.color(
@@ -75,8 +68,20 @@ def main(argc, argv):
             Alignment.Center
         )
 
-sys.argv.pop(0)
-argv: list[str] = sys.argv
-argc: int = len(sys.argv)
 
-main(argc, argv)
+def run(argc, argv):
+    commands_map: dict[tuple, callable] = {
+        ("tasks", "insert"): insert,
+        ("tasks", "done"): done,
+        ("tasks", "update"): update
+    }
+    command: callable = commands_map.get(tuple(argv), main)
+    command(argc, argv)
+
+if __name__ == "__main__":
+    sys.argv.pop(0)
+    argv: list[str] = sys.argv
+    argc: int = len(sys.argv)
+
+    run(argc, argv)
+
